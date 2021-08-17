fcdc6e563fdcb325d02c9173795b029d84595080
john kaizer <johnkaiser964@gmail.com>
2021-08-17 12:11:34 +0300

    added a Splash Screen. Added Google vision TextRecognizer

--- app/src/main/java/com/example/textscanner/MainActivity.java
+++ app/src/main/java/com/example/textscanner/MainActivity.java
@@ -6,21 +6,28 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.mlkit.vision.text.TextRecognizer;
-import com.google.mlkit.vision.text.internal.TextRecognizerImpl;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageActivity;
 import com.theartofdev.edmodo.cropper.CropImageView;
+
+
 
 import java.io.IOException;
 
@@ -51,6 +58,15 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View v) {
                     CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(MainActivity.this);
                 }});
+
+            button_copy.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String scanned_text = textView_data.getText().toString();
+                    copyToClipBoard(scanned_text);
+
+                }
+            });
         }
     }
 
@@ -63,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
                 Uri resultUri = result.getUri();
                 try {
                     bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
+                getTextFromImage(bitmap);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -70,7 +87,31 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-//    private void getTextFromImage(Bitmap bitmap){
-//        TextRecognizer recognizer = new Te
-//    }
+    private void getTextFromImage(Bitmap bitmap){
+        TextRecognizer recognizer = new TextRecognizer.Builder(this).build();{
+            if(!recognizer.isOperational()){
+                Toast.makeText(MainActivity.this, "An Error Occurred!!", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                SparseArray<TextBlock> textBlockSparseArray = recognizer.detect(frame);
+                StringBuilder stringBuilder= new StringBuilder();
+                for (int i=0; i<textBlockSparseArray.size(); i++){
+                    TextBlock textBlock = textBlockSparseArray.valueAt(i);
+                    stringBuilder.append(textBlock.getValue());
+                    stringBuilder.append("\n");
+                }
+                textView_data.setText(stringBuilder.toString());
+                button_capture.setText("Retake");
+                button_copy.setVisibility(View.VISIBLE);
+            }
+
+        }
+    }
+    private void copyToClipBoard(String text){
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Copied Data", text);
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(MainActivity.this, "Copied to clipboard", Toast.LENGTH_SHORT).show();
+    }
 }
